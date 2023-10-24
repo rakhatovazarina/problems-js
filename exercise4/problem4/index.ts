ac3309dbd6c5047a814879d4339b72fae180212e
Zarina Rakhatova <143261793+rakhatovazarina@users.noreply.github.com>
2023-10-24 14:28:58 +0600

    Update index.ts

--- exercise4/problem4/index.ts
+++ exercise4/problem4/index.ts
@@ -1,8 +1,31 @@
-// Update it as much as you want, just don't change the name
-export class Circle {}
+interface Shape {
+    allArea(): number;
+}
 
-// Update it as much as you want, just don't change the name
-export class Rectangle {}
+export class Circle implements Shape {
+    constructor(public radius: number) {}
 
-// Update it as much as you want, just don't change the name
-export function sumOfAllAreas() {}
+    allArea(): number {
+        return this.radius ** 2 * Math.PI;
+    }
+}
+
+export class Rectangle implements Shape {
+    constructor(public width: number, public height: number) {}
+
+    allArea(): number {
+        return this.width * this.height;
+    }
+}
+
+export function sumOfAllAreas(...shapes: Shape[]): number {
+    return shapes.reduce((total, shape) => total + shape.allArea(), 0);
+}
+
+const circle1 = new Circle(2);
+const circle2 = new Circle(3);
+const rect1 = new Rectangle(2, 4);
+const rect2 = new Rectangle(3, 2);
+
+console.log(sumOfAllAreas(circle1, circle2)); // ~ 40.84...
+console.log(sumOfAllAreas(circle1, rect1));   // ~ 20.84...
