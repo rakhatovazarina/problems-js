2d6cc6c65db39e54b28b61149ecaa2118c0a8c95
Zarina Rakhatova <143261793+rakhatovazarina@users.noreply.github.com>
2023-10-24 14:40:34 +0600

    Update index.ts

--- exercise4/problem7/index.ts
+++ exercise4/problem7/index.ts
@@ -1,3 +1,20 @@
-const is;
+class TypeChecker {
+    isBoolean(value: unknown): boolean {
+        return typeof value === "boolean";
+    }
 
+    isNumber(value: unknown): boolean {
+        return typeof value === "number";
+    }
+
+    isString(value: unknown): boolean {
+        return typeof value === "string";
+    }
+
+    isFunction(value: unknown): boolean {
+        return typeof value === "function";
+    }
+}
+
+const is = new TypeChecker();
 export default is;
