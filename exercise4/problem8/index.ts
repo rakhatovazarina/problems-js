b18687d05e8f3fb4414b2220aa26285f6e31510c
Zarina Rakhatova <143261793+rakhatovazarina@users.noreply.github.com>
2023-10-24 14:43:48 +0600

    Update index.ts

--- exercise4/problem8/index.ts
+++ exercise4/problem8/index.ts
@@ -1,3 +1,37 @@
-class Calculator {}
+class Calculator {
+    #result: number;
+
+    constructor(initValue: number = 0) {
+        this.#result = initValue;
+    }
+
+    add(value: number): this {
+        this.#result += value;
+        return this;
+    }
+
+    subtract(value: number): this {
+        this.#result -= value;
+        return this;
+    }
+
+    multiply(value: number): this {
+        this.#result *= value;
+        return this;
+    }
+
+    divide(value: number): this | string {
+        if (value === 0) {
+            return "Don't divide by zero!";
+        }
+        this.#result /= value;
+        return this;
+    }
+
+    logResult(): this {
+        console.log(this.#result);
+        return this;
+    }
+}
 
 export default Calculator;
