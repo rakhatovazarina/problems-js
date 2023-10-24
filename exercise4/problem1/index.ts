42151aa0b8db4e349162f2560ff39f6e6b3f2f63
Zarina Rakhatova <143261793+rakhatovazarina@users.noreply.github.com>
2023-10-24 14:13:30 +0600

    Update index.ts

--- exercise4/problem1/index.ts
+++ exercise4/problem1/index.ts
@@ -1,3 +1,24 @@
-class Person {}
+class Person {
+    #name: string = '';
+    #age: number = 0;
+
+
+    constructor(name: string, age:number){
+        this.#name = name;
+        this.#age = age;
+    }
+    compareAge(person: Person) {
+        if(this.#age > person.#age){
+            return `${person.#name} is younger than me.`
+        }
+        else if(this.#age < person.#age){
+            return `${person.#name} is older than me.`
+        }
+        else{
+            return `${person.#name} is the same age as me.`
+        }
+    }
+
+}
 
 export default Person;
