759519d7a4820701fb36476e7fc0564d8d4079c6
Zarina Rakhatova <143261793+rakhatovazarina@users.noreply.github.com>
2023-11-07 17:26:34 +0600

    Update index.ts

--- exercise5/problem3/index.ts
+++ exercise5/problem3/index.ts
@@ -1,3 +1,17 @@
-function printAsyncNumbers() {}
+function printAsyncNumbers(b: number) {
+  async function printNumberWithDelay(n: number) {
+    console.log(`after ${n} sec - ${n}`);
+  }
 
-export default printAsyncNumbers;
+  for (let n = 1; n <= b; n++) {
+    await new Promise((resolve) => {
+      setTimeout(async () => {
+        await printNumberWithDelay(n);
+        resolve();
+      }, n * 1000);
+    });
+  }
+}
+
+printAsyncNumbers(3);
+printAsyncNumbers(10);
