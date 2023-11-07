7780023cc7a892be1d3814969ec488fc14f69661
Zarina Rakhatova <143261793+rakhatovazarina@users.noreply.github.com>
2023-11-07 17:25:25 +0600

    Update index.ts

--- exercise5/problem2/index.ts
+++ exercise5/problem2/index.ts
@@ -1,3 +1,19 @@
-function printNumberInInterval() {}
+async function printNumberInInterval(b: number) {
+  async function anyfunc(n: number): Promise<void> {
+    console.log(`after 1 sec - ${n}`);
+  }
 
-export default printNumberInInterval;
+  let n = 1;
+  const delay = 1000;
+
+  const intervalId = setInterval(async () => {
+    await anyfunc(n);
+
+    if (n === b) {
+      clearInterval(intervalId);
+    }
+    n++;
+  }, delay);
+}
+
+printNumberInInterval(10);
