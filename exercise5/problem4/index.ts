f25c883f4ef3e8a152e4d6055a10d4c7adf683ab
Zarina Rakhatova <143261793+rakhatovazarina@users.noreply.github.com>
2023-11-07 17:28:31 +0600

    Update index.ts

--- exercise5/problem4/index.ts
+++ exercise5/problem4/index.ts
@@ -1,3 +1,50 @@
-class Timer {}
+class Timer {
+  private startTime: number;
+  private isPaused: boolean;
+  private elapsedTime: number;
+
+  constructor() {
+    this.startTime = 0;
+    this.isPaused = false;
+    this.elapsedTime = 0;
+  }
+
+  start() {
+    if (!this.startTime || this.isPaused) {
+      this.startTime = new Date().getTime() - (this.elapsedTime * 1000);
+      this.isPaused = false;
+      this.updateElapsedTime();
+    }
+  }
+
+  private updateElapsedTime() {
+    const currentTime = new Date().getTime();
+    this.elapsedTime = Math.floor((currentTime - this.startTime) / 1000);
+  }
+
+  pause() {
+    if (!this.isPaused) {
+      this.isPaused = true;
+      this.updateElapsedTime();
+    }
+  }
+
+  reset() {
+    this.startTime = new Date().getTime();
+    this.elapsedTime = 0;
+  }
+
+  log() {
+    console.log(this.formatTime());
+  }
+
+  private formatTime() {
+    const hours = Math.floor(this.elapsedTime / 3600);
+    const minutes = Math.floor((this.elapsedTime % 3600) / 60);
+    const seconds = this.elapsedTime % 60;
+
+    return `{ h: ${hours}, m: ${minutes}, s: ${seconds}, ms: 0 }`;
+  }
+}
 
 export default Timer;
