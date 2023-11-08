b793462644e422f524dbdf8ae1ead95dc8af4a18
Zarina Rakhatova <143261793+rakhatovazarina@users.noreply.github.com>
2023-11-08 21:30:42 +0600

    Update index.ts

--- exercise5/problem6/index.ts
+++ exercise5/problem6/index.ts
@@ -1,3 +1,24 @@
-async function fetchPokemon() {}
+async function fetchPokemon(pokemonName: string) {
+  try {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      return Promise.reject(new Error("Failed to fetch Pokémon data"));
+    }
+
+    const pokemon = await response.json();
+    return {
+      id: pokemon.id,
+      name: pokemon.name,
+      height: pokemon.height,
+      weight: pokemon.weight,
+      image: pokemon.sprites.front_default,
+    };
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
 
 export default fetchPokemon;
