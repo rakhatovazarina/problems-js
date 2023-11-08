1b678a6856eaafbcf525f1696d0451712368c7e8
Zarina Rakhatova <143261793+rakhatovazarina@users.noreply.github.com>
2023-11-08 21:32:39 +0600

    Update index.ts

--- exercise5/problem7/index.ts
+++ exercise5/problem7/index.ts
@@ -1,5 +1,32 @@
-async function fetchPokemons() {
-  // Your code
+async function fetchPokemon(name) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch Pokémon data");
+  }
+
+  const pokemon = await response.json();
+  return {
+    id: pokemon.id,
+    name: pokemon.name,
+    height: pokemon.height,
+    weight: pokemon.weight,
+    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`,
+  };
+}
+
+async function fetchPokemons(names) {
+  try {
+    const promises = names.map(async (name) => {
+      return await fetchPokemon(name);
+    });
+
+    const pokemonData = await Promise.all(promises);
+    return pokemonData;
+  } catch (err) {
+    throw new Error("Something went wrong");
+  }
 }
 
 export default fetchPokemons;
