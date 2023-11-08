b4af15675ff5cf68dcf1ef394ce7455d548f5b4f
Zarina Rakhatova <143261793+rakhatovazarina@users.noreply.github.com>
2023-11-08 21:35:13 +0600

    Update index.ts

--- exercise5/problem8/index.ts
+++ exercise5/problem8/index.ts
@@ -1,5 +1,35 @@
-async function fetchCharacterWithMovies() {
-  // Your code
-}
+async function fetchCharacterWithMovies(characterId: number): Promise<any> {
+  const apiUrl = `https://swapi.dev/api/people/${characterId}/`;
 
-export default fetchCharacterWithMovies;
+  try {
+    // Fetch character information
+    const characterResponse = await fetch(apiUrl);
+
+    if (!characterResponse.ok) {
+      throw new Error("Failed to fetch character information");
+    }
+
+    const characterData = await characterResponse.json();
+
+    // Fetch films and handle errors for each film fetch
+    const films = await Promise.all(
+      characterData.films.map(async (filmUrl: string) => {
+        const filmResponse = await fetch(filmUrl);
+
+        if (!filmResponse.ok) {
+          throw new Error(`Failed to fetch film information for ${characterData.name}`);
+        }
+
+        const filmData = await filmResponse.json();
+        return filmData.title;
+      })
+    );
+
+    const result = {
+      name: characterData.name,
+      films: films,
+    };
+
+    return result;
+  } catch
+
