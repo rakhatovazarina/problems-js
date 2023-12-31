6953395ad75fed1cdbef73ceb92367a176e8b0e1
Zarina Rakhatova <143261793+rakhatovazarina@users.noreply.github.com>
2023-12-31 19:50:42 +0600

    Add files via upload

---
+++ exercise9/exercise9/file/header.svelte
@@ -0,0 +1,57 @@
+<script context="module" lang="ts">
+  export interface UserData {
+    login: string;
+    name: string;
+    avatar_url: string;
+    html_url: string;
+    bio: string;
+    created_at: string;
+    location?: string;
+    twitter_username?: string;
+  }
+</script>
+
+<script>
+  export let userData: UserData;
+</script>
+
+<style>
+</style>
+
+<section class="main-2">
+  {#if userData != null}
+    <img class="main-img" src={userData.avatar_url} alt="cat">
+    <h1 class="main-octo">
+      {userData.name}
+      <p class="main-octocat">
+        <a href={userData.html_url} target="_blank">{userData.login}</a>
+      </p>
+      <p class="main-bio">{userData.bio}</p>
+    </h1>
+    <p class="main-data"> Joined {userData.created_at}</p>
+  {:else}
+    <img class="main-img" src="Bitmap.svg" alt="cat">
+    <h1 class="main-octo">
+      The Octocat
+      <p class="main-octocat">@octocat</p>
+      <p class="main-bio">This profile has no bio</p>
+    </h1>
+    <p class="main-data"> Joined 25 Jan 2011</p>
+  {/if}
+</section>
+
+<footer class="footer">
+  <div class="footer-1">
+    {#if userData != null}
+      <p><img src="001-office.svg" alt="location">{userData.location}</p>
+      <p><img src="002-url.svg" alt="twitter">{userData.twitter_username}</p>
+    {:else}
+      <p><img src="003-pin.svg" alt="location"> San Francisco</p>
+      <p><img src="004-twitter.svg" alt="twitter"> Not Available</p>
+    {/if}
+  </div>
+  <div class="footer-2">
+    <p><img src="003-pin.svg" alt="link"> https://github.blog</p>
+    <p><img src="004-twitter.svg" alt="office"> @github</p>
+  </div>
+</footer>
---
+++ exercise9/exercise9/src/app.svelte
@@ -0,0 +1,139 @@
+<script>
+  import header from '../file/header.svelte'
+	  let username = '';
+	  let userData =null;
+	
+	  async function func() {
+		try{
+		const response = await fetch( `https://api.github.com/users/${username}`);
+		const jsonn = await response.json();
+		if (!response.ok) {
+		  await Promise.reject(new Error(`Error 37 reject`));
+		}
+	  userData = jsonn;
+	  } catch (error) {
+	  otherfunc()
+	}
+	
+	}
+	function otherfunc() {
+	  console.log('error catch');
+	}
+</script>
+<body>
+  
+<header>
+	<div class="header">
+	<h2 class="devfinder">devfinder</h2>
+	<h3 class="dark">DARK 
+	  <img class="moon" src="moon.svg" alt="moon">
+	</h3>
+	</div>
+
+	<form on:submit|preventDefault={func} class="search">
+	  <div class="search-2">
+	<img  src="Combined Shape.svg" alt="shape">
+	  <input bind:value={username} class="search-input" type="text"  placeholder="Search GitHub username…"/>
+	</div>
+	   <button class="search-btn" type="submit">Search</button>
+</form>
+
+  </header>
+  <header class="main">
+  <header {userData}/>
+  <header/>
+</body>
+<style>
+  body{
+    padding: 9rem 22.19rem ;
+   background: #F6F8FF; 
+}
+.main{
+
+margin-top: 1.5rem;
+width: 45.625rem;
+height: 26.1875rem;
+flex-shrink: 0;
+border-radius: 0.9375rem;
+background: #FEFEFE;
+
+box-shadow: 0px 16px 30px -10px rgba(70, 96, 187, 0.20);
+}
+  .header{
+    display: inline-flex;
+   justify-content: center;
+   align-items: center;
+   gap: 32.75rem;
+   }
+
+   .devfinder{
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    gap: 32.75rem;
+   color: #222731;
+   font-family: 'Space Mono', monospace;
+   font-size: 1.625rem;
+   font-style: normal;
+   font-weight: 700;
+   line-height: normal;
+   }
+   .dark{
+   
+   color: #697C9A;
+   text-align: right;
+   font-family: 'Space Mono', monospace;
+   font-size: 0.8125rem;
+   font-style: normal;
+   font-weight: 700;
+   line-height: normal;
+   letter-spacing: 0.15625rem;
+   }
+   .moon{
+   padding-left: 1rem;
+   }
+   
+   
+   .search{
+       display: flex;
+       justify-content: center;
+       align-items: center;
+       gap: 17.5rem;
+       width: 45.62rem;
+   border-radius: 0.9375rem;
+   background: #FEFEFE;
+   box-shadow: 0px 16px 30px -10px rgba(70, 96, 187, 0.20);
+   }
+   
+   
+   .search-input{
+       border: none;
+   margin: 1.38rem;
+   color: #4B6A9B;
+   font-family: Space Mono;
+   font-size: 1.125rem;
+   font-style: normal;
+   font-weight: 400;
+   line-height: 1.5625rem; /* 138.889% */
+   }
+   .search-btn{
+   width: 6.625rem;
+   height: 3.125rem;
+   flex-shrink: 0;
+   border-radius: 0.625rem;
+   background: #0079FF;
+   
+   color: #FFF;
+   font-family: Space Mono;
+   font-size: 1rem;
+   font-style: normal;
+   font-weight: 700;
+   line-height: normal;
+   }
+  </style>
+
+
+
+  
+
+ 
