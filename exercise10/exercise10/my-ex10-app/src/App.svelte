fd67c69eb718ea410a2a91107244baa74930bd34
Zarina Rakhatova <143261793+rakhatovazarina@users.noreply.github.com>
2024-01-15 00:59:04 +0600

    Add files via upload

---
+++ exercise10/exercise10/my-ex10-app/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import CardForm from './lib/CardForm.svelte';
+
+  import Thanks from './lib/Thanks.svelte';
+  let showThankYou = false;
+
+  const handleThankYou = () => {
+    showThankYou = true;
+    console.log('handleThankYou');
+  };
+</script>
+
+<main>
+  {#if showThankYou}
+    <div class="thank_you">
+      <Thanks />
+    </div>
+  {:else}
+    <div class="card">
+      <CardForm myCallThanks={handleThankYou} />
+    </div>
+  {/if}
+</main>
---
+++ exercise10/exercise10/my-ex10-app/src/lib/Thanks.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<section>
+  <h2>THANK YOU!</h2>
+
+  <div class="note">We’ve added your card details</div>
+</section>
+
+<button>Continue</button>
